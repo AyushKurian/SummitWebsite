349f74c18f0becc2a9aa180f96f4aefd00ead1da
angleju329 <79028642+angleju329@users.noreply.github.com>
2021-02-15 14:29:13 -0500

    updated contact page

--- fcf-assets/fcf.process.php
+++
@@ -1,9 +0,0 @@
-<?php
-// ************************************
-// This file is part of a package from:
-// www.freecontactform.com
-// See license for details
-// ************************************
-
-require dirname(__FILE__).'/'.'fcf.config.php';
-require dirname(__FILE__).'/'.'process.php';
